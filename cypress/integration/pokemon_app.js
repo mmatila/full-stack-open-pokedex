ae91260fd120f14708f4932aef960f2b7e89af32
Manu Matila <manu.matila1997@gmail.com>
2021-10-07 10:56:41 +0300

    11.9

--- cypress/integration/pokemon_app.js
+++ cypress/integration/pokemon_app.js
@@ -6,8 +6,7 @@ describe('Pokedex', function () {
   })
 
   it('pokemon page can be navigated to', () => {
-    cy.visit('http://localhost:5000')
-    cy.contains('ivysaur')
-
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
   })
 })
